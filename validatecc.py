3f52cb5dbcd5369f4d5b75dfd7b4bc3c8cf56059
lionelster <lionelster@gmail.com>
2014-10-08 15:08:53 +0800

    change a var name to a non-builtin name

--- validatecc.py
+++ validatecc.py
@@ -5,12 +5,12 @@ def validatecc(num):
 	try:
 		digits = [int(char) for char in num]
 	except ValueError: return False
-	sum = 0
+	total = 0
 	for i in range(0,15,2): # for every pair of digits
 		x = digits[i]*2 # first digit of pair * 2
 		x = x if x < 10 else x-10+1 # if result is double-digit, sum up its digits
-		sum += x + digits[i+1] # add second digit of pair
-	return sum%10 == 0 # true if divisible by 10
+		total += x + digits[i+1] # add second digit of pair
+	return total%10 == 0 # true if divisible by 10
 
 if __name__=='__main__':
 	print('Valid!' if validatecc(input('Enter 16-digit credit card number: ').strip()) else 'Invalid!')
